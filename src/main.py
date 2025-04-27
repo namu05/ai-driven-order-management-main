e3ec3bf8a0feef54979b2948a2d807e3b4e0fdfb
namu_05 <naman.katuva@gmail.com>
2025-04-28 00:57:20 +0530

    Integrate langsmith for latency, token usage and llm hit

--- src/main.py
+++ src/main.py
@@ -32,22 +32,22 @@ def main():
         return fetch_order_data(session)
         
 # Test cancel order
-    print("\nTesting Cancel Order:")
-    user_query = "I wish to cancel order_id 4ed4146c-fd2f-4593-9c49-27f33fe2dba6"
+    # print("\nTesting Cancel Order:")
+    # user_query = "I wish to cancel order_id 4ed4146c-fd2f-4593-9c49-27f33fe2dba6"
+    # for chunk in agent.stream(
+    #     {"messages": [("user", user_query)]},
+    #     stream_mode="values",
+    # ):
+    #     chunk["messages"][-1].pretty_print()
+    
+#Test place order
+    print("\nTesting Place Order:")
+    user_query = "David Brown : I wish to place order for item_28 with order quantity as 4"
     for chunk in agent.stream(
         {"messages": [("user", user_query)]},
         stream_mode="values",
     ):
         chunk["messages"][-1].pretty_print()
-    
-#Test place order
-        # print("\nTesting Place Order:")
-        # user_query = "David Brown : I wish to place order for item_28 with order quantity as 4"
-        # for chunk in agent.stream(
-        #     {"messages": [("user", user_query)]},
-        #     stream_mode="values",
-        # ):
-        #     chunk["messages"][-1].pretty_print()
 
 #Test assitant        
         # user_query = "How cam i contact customer support?"
